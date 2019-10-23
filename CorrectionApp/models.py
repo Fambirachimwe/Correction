86ea012d34977fe1a7474927e89287d4f79802de
Fambirachimwe <t.svaku@gmail.com>
2019-10-23 08:38:26 +0200

    'initial'

---
+++ CorrectionApp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib import auth
+from django.urls import reverse_lazy, reverse
+from datetime import datetime
+# from django.utils.timezone.now import
+
+
+
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def __str__(self):
+        return "{}".format(self.username)
+
+
+class Car(models.Model):
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='car_images', blank=True)
+    daily_rent = models.IntegerField()
+
+    is_available = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse('car-details', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.name
+
+
+class Bookings(models.Model):
+
+    car = models.ForeignKey(Car, related_name='car', on_delete=models.CASCADE)
+    booking_start_date = models.DateField(default=datetime.now())
+    booking_end_date = models.DateField(default=datetime.now())
+    is_approved = models.BooleanField(null=True)
+    # add the username booking
+
+
+
+
+
+
+
+
